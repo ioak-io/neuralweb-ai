da65c35dd7db894bf64cc3375d3d1533e9c108ea
Arunkumar Selvaraj <arun@ioak.io>
2023-04-14 18:25:56 +0530

    deployment config

--- src/main/utils/minio_utils.py
+++ src/main/utils/minio_utils.py
@@ -5,6 +5,24 @@ import joblib
 
 BUCKET_NAME = "neuralweb-ai"
 
+MINIO_URL = os.environ.get('MINIO_URL')
+secure = True
+if MINIO_URL is None:
+    MINIO_URL = 'localhost'
+    secure = False
+
+MINIO_PORT = os.environ.get('MINIO_PORT')
+if MINIO_PORT is None:
+    MINIO_PORT = '8090'
+
+MINIO_ACCESS_KEY = os.environ.get('MINIO_ACCESS_KEY')
+if MINIO_ACCESS_KEY is None:
+    MINIO_ACCESS_KEY = 'systemadmin'
+
+MINIO_SECRET = os.environ.get('MINIO_SECRET')
+if MINIO_SECRET is None:
+    MINIO_SECRET = 'systemadmin'
+
 # https://medium.com/@erkansirin/storing-and-retrieving-scikit-learn-model-to-from-s3-f808457bc624
 
 
@@ -18,10 +36,10 @@ def load(path):
 
 def _get_minio_client():
     client = Minio(
-        "localhost:8085",
-        access_key="systemadmin",
-        secret_key="systemadmin",
-        secure=False
+        MINIO_URL + ":" + MINIO_PORT,
+        access_key=MINIO_ACCESS_KEY,
+        secret_key=MINIO_SECRET,
+        secure=secure
     )
 
     found = client.bucket_exists(BUCKET_NAME)
